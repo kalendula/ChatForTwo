fe9e9fa16d615a5063a862d425f2429a817311f0
kale ndula <kirochkin.g@gmail.com>
2019-01-31 23:29:22 +0200

    now using sql for data

--- server.js
+++ server.js
@@ -1,97 +1,106 @@
 const fs = require('fs')
 var express = require("express");
+const sqlite3 = require('sqlite3').verbose();
 var app = express();
+
 
 app.use(express.static(__dirname + "/public"));
 
 app.get("/users", function(req, res){
-    var content = fs.readFileSync("users.json", "utf8");
-    var users = JSON.parse(content);
+    let db = new sqlite3.Database(__dirname+'\\chatHistory\\chatHistory.db', (err) => {
+        if (err) {console.error(err.message);}
+    });
+    let users = [];
+    db.each(`SELECT * FROM users`, (err, row) => {
+        if (err){ throw err;}
+        var e = {nick : row.nick, pass : row.password};
+        users.push(e);});
+        db.close((err) => {
+            if (err) {return console.error(err.message);}
+            
     res.send(users);
-
+          });
 });
 
 app.get("/enter", function(req, res){
     var nick = req.param('nick');
     var password = req.param('pass');
+    var isReg = req.param('isRegister');
     res.sendFile(__dirname+"/public/chat.html");
-    var content = fs.readFileSync("users.json", "utf8");
-    var users = JSON.parse(content);
-    var user = null;
-    for(var i=0; i<users.length; i++){
-        if(users[i].nick===nick){
-            user = users[i];
-            break;
-        }
-    }
-    if(user){}
-    else{
-        var user = {id: 1, nick: nick, pass: password};
-        var data = fs.readFileSync("users.json", "utf8");
-        var users = JSON.parse(data);
-        var id = Math.max.apply(Math,users.map(function(o){return o.id;}))
-        user.id = id+1;
-        users.push(user);
-        var data = JSON.stringify(users);
-        fs.writeFileSync("users.json", data);
+    if (isReg==="1"){
+    let db = new sqlite3.Database(__dirname+'\\chatHistory\\chatHistory.db', (err) => {
+        if (err) {console.error(err.message);}
+    });
+    db.run(`INSERT INTO users(nick, password) VALUES('${nick}','${password}')`); 
+    db.run(`Create table ${nick} (talker text, message text, time number)`);   
+    db.close((err) => {
+        if (err) {return console.error(err.message);}
+      });
     }
 });
 
 app.get("/talker",  function (req, res) {
     var from=req.param('from');
     var to=req.param('to');
-    var path1 = __dirname+"/chatHistory/"+from+"-"+to+".json";
-    var path2 = __dirname+"/chatHistory/"+to+"-"+from+".json";
     var hsr=[];
-    if (fs.existsSync(path1)){
-    var content = fs.readFileSync(path1, "utf8");
-    hsr = JSON.parse(content);
-    }
-    else {
-        var str = {from: "sys", time: Date.now(), msg: "your history is clear!"};
-        hsr.push(str);
-        var data = JSON.stringify(hsr);
-        fs.appendFileSync(path1,data);
-        fs.appendFileSync(path2,data);
-    }
-    res.send(hsr);
+    let db = new sqlite3.Database(__dirname+'\\chatHistory\\chatHistory.db', (err) => {
+        if (err) {console.error(err.message);}
+    });
+    db.each(`SELECT * FROM ${from} where talker='${to}'`, (err, row) => {
+        if (err){ throw err;}
+        var e = {time : row.time, from : row.talker, message: row.message};
+        hsr.push(e);});
+    db.each(`SELECT * FROM ${to} where talker='${from}'`, (err, row) => {
+        if (err){ throw err;}
+        var e = {time : row.time, from : row.talker, message: row.message};
+        hsr.push(e);});
+        db.close((err) => {
+            hsr.sort(function (a,b){return Number(a.time)-Number(b.time)})
+            res.send(hsr);
+            if (err) {return console.error(err.message);}
+            
+          });
+    
 });
 
 app.post("/message",  function (req, res) {
     var from=req.param('from');
     var to=req.param('to');
     var msg=req.param('msg');
-    var path1 = __dirname+"/chatHistory/"+from+"-"+to+".json";
-    var path2 = __dirname+"/chatHistory/"+to+"-"+from+".json";
-    var content = fs.readFileSync(path1, "utf8");
-    var msgs = JSON.parse(content);
-    var str = {from: from, time: Date.now(), msg: msg};
-    msgs.push(str);
-    var data = JSON.stringify(msgs);
-    fs.writeFileSync(path1,data);
-    fs.writeFileSync(path2,data);
-    res.send(msgs[msgs.length-1]);
+    let db = new sqlite3.Database(__dirname+'\\chatHistory\\chatHistory.db', (err) => {
+        if (err) {console.error(err.message);}
+    });
+    db.run(`INSERT INTO ${to} (talker,message, time) VALUES('${from}','${msg}',${Date.now()})`); 
+    db.close((err) => {
+        if (err) {return console.error(err.message);}
+      });
+    var e = {msg: msg, from: from, time: Date.now()};
+    res.send(e);
 });
 
 app.get("/update",  function (req, res) {
-    var from=req.param('from');
-    var to=req.param('to');
-    var time=req.param('time');
-    var path1 = __dirname+"/chatHistory/"+from+"-"+to+".json";
-    var content = fs.readFileSync(path1, "utf8");
-    var msgs = JSON.parse(content);
-    var update = [];
-    for (let i = 0; i < msgs.length;i++){
-        if (msgs[i].time>time){
-            update.push(msgs[i]);
-        }
-    }
-    if (update.length==0){
-        res.send("");
-    }
-    else {
-        res.send(update);
-    }
+        var time = Date.now();
+        var from=req.param('from');
+        var to=req.param('to');
+        var lastTime=req.param('time');
+        var hsr=[];
+        let db = new sqlite3.Database(__dirname+'\\chatHistory\\chatHistory.db', (err) => {
+            if (err) {console.error(err.message);}
+        });
+        db.each(`SELECT * FROM ${from} where talker='${to}' and time>${lastTime}`, (err, row) => {
+            if (err){ throw err;}
+            var e = {time : row.time, from : row.talker, msg: row.message};
+            hsr.push(e);});
+        db.each(`SELECT * FROM ${to} where talker='${from}'  and time>${lastTime}`, (err, row) => {
+            if (err){ throw err;}
+            var e = {time : row.time, from : row.talker, msg: row.message};
+            hsr.push(e);});
+            db.close((err) => {
+                hsr.sort(function (a,b){return Number(a.time)-Number(b.time)})
+                res.send(hsr);
+                if (err) {return console.error(err.message);}
+                
+              });
     
 });
 
